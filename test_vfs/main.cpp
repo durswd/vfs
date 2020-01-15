2c9ecaaa368c5984cc5d45ffb693a8a7fd39b184
durswd <swda.durl@gmail.com>
2020-01-15 19:24:53 +0900

    Update codes

--- test_vfs/main.cpp
+++ test_vfs/main.cpp
@@ -1,38 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <stereotgv/stereotgv.h>
 
-#if _WIN64
-#define LIB_PATH "D:/dev/lib64/"
-#define CV_LIB_PATH "D:/dev/lib64/"
-#else
-#define LIB_PATH "D:/dev/staticlib32/"
-#endif
-
-#ifdef _DEBUG
-#define LIB_EXT "d.lib"
-#else
-#define LIB_EXT ".lib"
-#endif
-
-#define CUDA_LIB_PATH "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/lib/x64/"
-#pragma comment(lib, CUDA_LIB_PATH "cudart.lib")
-
-#define CV_VER_NUM CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
-#pragma comment(lib, LIB_PATH "opencv_core" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_highgui" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_videoio" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_imgproc" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_calib3d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_xfeatures2d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_optflow" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_imgcodecs" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_features2d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_tracking" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_flann" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_cudafeatures2d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_cudaimgproc" CV_VER_NUM LIB_EXT)
-
-#pragma comment(lib, LIB_PATH "opencv_video" CV_VER_NUM LIB_EXT)
 void showDepthJet(std::string windowName, cv::Mat image, float maxDepth, bool shouldWait = true) {
 	cv::Mat u_norm, u_gray, u_color;
 	u_norm = image * 256.0f / maxDepth;
@@ -96,6 +64,13 @@ int main() {
 	clock_t timeElapsed = (clock() - start);
 	std::cout << "time: " << timeElapsed << " ms ";
 	cv::writeOpticalFlow("disparity.flo", disparity);
+
+	std::vector<cv::Mat> planes;
+	cv::split(disparity, planes);
+
+	cv::imshow("opt1", planes[0] / 16.0f);
+	cv::imshow("opt2", planes[1] / 16.0f);
+
 	cv::waitKey();
 	return 0;
 }
